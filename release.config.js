6e48867c69be638d16e8b702022a81b44115764d
rcn <rnalus@gmail.com>
2022-04-15 13:25:03 +0800

    refs2

--- release.config.js
+++ release.config.js
@@ -1,8 +1,9 @@
 module.exports = {
-	branches: "master",
-	repositoryUrl: "https://github.com/rnalus2022gh/react-app",
-	plugins: [
-		"@semantic-release/commit-analyzer", 
-        "@semantic-release/release-notes-generator", 
-		["@semantic-release/github"
-}
+  branches: "master",
+  repositoryUrl: "https://github.com/rnalus2022gh/react-app",
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/github",
+  ],
+};
